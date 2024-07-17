777ba6b93a56a1ef5077468e1722f5b0abd52909
Chhin Whatana <chhinwathana@gmail.com>
2024-07-17 16:31:45 +0700

    fix contact

--- src/component/module/contact/contact.tsx
+++ src/component/module/contact/contact.tsx
@@ -3,7 +3,7 @@ import ContactContent from './components/contact-content'
 
 const Contact = () => {
   return (
-    <div className="m-auto mt-5  flex min-h-screen max-w-screen-2xl items-center justify-center bg-[#8ACEC0]">
+    <div className="m-auto  flex min-h-screen max-w-screen-2xl items-center justify-center bg-[#8ACEC0]">
       <div className="w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1280px]">
         <ContactContent />
       </div>
