e1bba269f68e98aa7f62619fc6c3c11429a57654
Sovanrath <sovanrathsoem@gmail.com>
2024-07-17 10:19:27 +0700

    feat: Investment index image

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -20,3 +20,60 @@ export const logo = [
         Image: "/images/logoaifarm.avif",
     }
 ] 
+
+export const Investments =[
+    {
+        id: "1",
+        Image: "/images/AgriTech.svg",
+        title: "AgriTech",
+        description: "agribusiness and industrial zones"
+    },
+    {
+        id: "2",
+        Image: "/images/Estate.svg",
+        title: "Real Estate",
+        description: "real estates and natural resort zones"
+    },
+    {
+        id: "3",
+        Image: "/images/BioTech.svg",
+        title: "BioTech",
+        description: "biotechnology and nano materials"
+    },
+    {
+        id: "4",
+        Image: "/images/AI.svg",
+        title: "AI & Software",
+        description: "next generation artificail intelligence and saas"
+    },
+    {
+        id: "5",
+        Image: "/images/FinTech.svg",
+        title: "FinTech",
+        description: "banking, gold, stock, blockchain, crypto, nft"
+    },
+    {
+        id: "6",
+        Image: "/images/Knowledge.svg",
+        title: "Knowledge",
+        description: "learning resources and broadcasting investment"
+    },
+    {
+        id: "7",
+        Image: "/images/EdTech.svg",
+        title: "EdTech",
+        description: "enables personalized learning experiences"
+    },
+    {
+        id: "8",
+        Image: "/images/CleanTech.svg",
+        title: "CleanTech",
+        description: "sustainable solutions to pressing environmental challenges"
+    },
+    {
+        id: "9",
+        Image: "/images/Robotics.svg",
+        title: "Robotics",
+        description: "enhances efficiency by automating repetitive tasks"
+    }
+]
