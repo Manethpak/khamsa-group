0b7a320f9c0d565f849dac02cd32b040d50ca043
manethpak <manethpak00@gmail.com>
2024-08-15 15:05:39 +0700

    feat: add remote image pattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.dreamslab.dev',
+      },
+    ],
+  },
+}
 
 export default nextConfig
