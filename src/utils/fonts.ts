3d71e021121f27e25aa481e2d191fcab9264b9fd
manethpak <manethpak00@gmail.com>
2024-09-03 11:30:36 +0700

    chore: add font weights

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -2,19 +2,19 @@ import { Lato, Libre_Baskerville, Noto_Serif_Khmer } from 'next/font/google'
 
 const lato_init = Lato({
   subsets: ['latin'],
-  weight: ['400'],
+  weight: ['100', '300', '400', '700', '900'],
   variable: '--font-lato',
 })
 
 const libre_init = Libre_Baskerville({
   subsets: ['latin'],
-  weight: ['400'],
+  weight: ['400', '700'],
   variable: '--font-libre',
 })
 
 const noto_serif_khmer_init = Noto_Serif_Khmer({
   subsets: ['latin'],
-  weight: ['500'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   variable: '--font-noto-serif-khmer',
 })
 
