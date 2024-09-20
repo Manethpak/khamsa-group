49c1a4a88f8f1d77f877b42c53e6cb8d15ece038
Chhin Whatana <chhinwathana@gmail.com>
2024-09-20 10:46:36 +0700

    fix: responsive and click funtion in our journey

--- src/component-v2/module/homepage/ourjourney-section/our-journey.tsx
+++ src/component-v2/module/homepage/ourjourney-section/our-journey.tsx
@@ -11,7 +11,7 @@ const OurJourney = () => {
   return (
     <section className="mx-auto flex w-full max-w-screen-2xl flex-col items-center px-5 md:px-10 lg:px-24">
       <motion.div className="flex w-full max-w-screen-2xl flex-col gap-10">
-        <h1 className="text-start  font-manrope text-2xl font-semibold text-secondPrimary sm:text-3xl md:text-4xl">
+        <h1 className="text-start font-manrope text-2xl font-semibold text-secondPrimary sm:text-3xl md:text-4xl">
           OUR JOURNEY
         </h1>
 
@@ -30,17 +30,19 @@ const OurJourney = () => {
             <motion.div
               key={index}
               onClick={() => setSelectedPhase(phase)}
-              className={`border-white-100 flex flex-col items-center justify-center bg-white transition-colors duration-300 focus:bg-secondPrimary focus:font-extrabold focus:text-white active:bg-secondPrimary sm:text-base md:text-xl lg:text-2xl ${selectedPhase === phase ? 'bg-secondPrimary font-semibold text-white' : 'bg-transparent text-green-700'}`}
-              style={{ width: '173px', height: '69px' }}
+              className={`flex flex-col items-center justify-center transition-colors duration-300 sm:text-base md:text-xl lg:text-2xl ${selectedPhase === phase ? 'bg-secondPrimary font-bold text-white' : 'bg-white font-normal text-secondPrimary'}`}
+              style={{
+                width: '173px',
+                height: '69px',
+                border: 'white',
+              }}
               variants={{
                 hidden: { opacity: 0, y: 75 },
                 visible: { opacity: 1, y: 0 },
               }}
               whileTap={{ scale: 1 }}
             >
-              <h1 className="font-manrope text-2xl text-[22px] font-light hover:font-semibold">
-                {phase}
-              </h1>
+              <h1 className="font-manrope text-2xl text-[22px]">{phase}</h1>
             </motion.div>
           ))}
         </motion.div>
