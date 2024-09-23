701ccbcdc3478bef5cef8316d6ce4d7030e54888
manethpak <manethpak00@gmail.com>
2024-09-23 16:44:16 +0700

    fix: fix build error

--- src/app/(v2)/v2/contact/page.tsx
+++ src/app/(v2)/v2/contact/page.tsx
@@ -16,12 +16,12 @@ interface Props {
 }
 
 const iconMapping = {
-  facebook: GrFacebookOption, 
+  facebook: GrFacebookOption,
   linkedin: GrLinkedinOption,
   youtube: FaTelegramPlane,
 }
 
-const page = () => {
+const ContactPage = () => {
   const { data } = useContact()
 
   const contact = data as {
@@ -34,13 +34,14 @@ const page = () => {
   const link: Props[] = contact?.link || []
 
   const [showArrow, setShowArrow] = useState<string | null>(null)
+
   return (
-    <div className="flex w-full flex-col items-center justify-center py-20 subtitle">
+    <div className="subtitle flex w-full flex-col items-center justify-center py-20">
       <Motion className="h-fit w-full max-w-screen-2xl px-5 md:px-10 lg:px-24">
         <div className="flex h-full w-full flex-col-reverse gap-6 gap-y-14 lg:flex-row">
           <div className="h-full w-full lg:w-fit">
             <iframe
-              className=" h-80 w-full lg:h-[580px] lg:w-[760px] lg:max-w-lg xl:max-w-full"
+              className="h-80 w-full lg:h-[580px] lg:w-[760px] lg:max-w-lg xl:max-w-full"
               allowFullScreen
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
@@ -48,7 +49,7 @@ const page = () => {
             ></iframe>
           </div>
           <div className="flex w-full max-w-sm flex-col gap-6">
-            <div className="w-full flex flex-col gap-6">
+            <div className="flex w-full flex-col gap-6">
               <div className="space-y-3">
                 <h1 className="title">Get in touch</h1>
                 <p>
@@ -92,22 +93,24 @@ const page = () => {
                 ))}
               </div>
             </div>
-            <div className="flex flex-col gap-4 ">
-                <h2 className="title">Follow us on:</h2>
-                <div className="flex gap-4">
-              {(contact?.social_link || []).map((data) => {
-                const IconComponent = iconMapping[data.icon_name]
-                return (
-                  <div key={data.title} className="border rounded-full justify-center items-center flex size-12">
-                    <Link target="_blank" href={`${data.link}`}>
-                      <IconComponent className="w-6 h-6"/>
-                    </Link>
-                  </div>
-                )
-              })}
-            </div>
+            <div className="flex flex-col gap-4">
+              <h2 className="title">Follow us on:</h2>
+              <div className="flex gap-4">
+                {(contact?.social_link || []).map((data) => {
+                  const IconComponent = iconMapping[data.icon_name]
+                  return (
+                    <div
+                      key={data.title}
+                      className="flex size-12 items-center justify-center rounded-full border"
+                    >
+                      <Link target="_blank" href={`${data.link}`}>
+                        <IconComponent className="h-6 w-6" />
+                      </Link>
+                    </div>
+                  )
+                })}
               </div>
-            
+            </div>
           </div>
         </div>
       </Motion>
@@ -115,4 +118,4 @@ const page = () => {
   )
 }
 
-export default page
+export default ContactPage
