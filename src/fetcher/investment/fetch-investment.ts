83dd2da531ec923853adca356660136b643439c9
Chhin Whatana <chhinwathana@gmail.com>
2024-10-07 09:55:16 +0700

    feat: projectdetail

--- src/fetcher/investment/fetch-investment.ts
+++ src/fetcher/investment/fetch-investment.ts
@@ -1,7 +1,7 @@
 import directus from '@/lib/directus'
 import { readSingleton } from '@directus/sdk'
 
-export async function fetchInvestment() {
+export async function fetchInvestment(slug: string) {
   return directus.request(
     readSingleton('Investment',{
       // @ts-expect-error
