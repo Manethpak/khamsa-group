57b99ecb86747e444862a91ff82baa53dfa932bb
Sovanrath <sovanrathsoem@gmail.com>
2024-10-14 17:08:56 +0700

    fix: link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import HomeSection from '@/component/module/homepage/home-section/home'
 import OurSuccess from '@/component/module/homepage/stats/our-success'
-import Investment from '@/component/module/homepage/investment-section/investment'
 import OurJourney from '@/component/module/homepage/our-journey-section/our-journey'
 import BlogSection from '@/component/module/homepage/blogs-section/blogs'
 import { fetchHero } from '@/fetcher/hero/fetch-hero'
 import { fetchBlogs } from '@/fetcher/blog/fetch-blog'
 import { fetchInvestment } from '@/fetcher/investment/fetch-investment'
 import { fetchSuccess } from '@/fetcher/about/success/fetch-success'
+import Company from '@/component/module/homepage/company-section/company'
 export const revalidate = 300
 
 export default async function Home() {
@@ -19,7 +19,7 @@ export default async function Home() {
     <div>
       <HomeSection data={heroData} />
       <OurSuccess data={successData} />
-      <Investment data={investmentData} />
+      <Company data={investmentData} />
       <OurJourney />
       <BlogSection data={blogs} />
     </div>
