d1d43d5e394a20485ffc7c1ec575ebe80500f71d
Sovanrath <sovanrathsoem@gmail.com>
2024-10-14 16:04:17 +0700

    fix: format

--- src/fetcher/company/fetch-company.ts
+++ src/fetcher/company/fetch-company.ts
@@ -9,7 +9,12 @@ export async function fetchCompany(option?: Option) {
   return directus.request(
     readItems('Company', {
       // @ts-expect-error
-      fields: ['*', 'category.Category_id.title', 'logo','company_description'],
+      fields: [
+        '*',
+        'category.Category_id.title',
+        'logo',
+        'company_description',
+      ],
       limit: option?.limit || -1,
     })
   )
