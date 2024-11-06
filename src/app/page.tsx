adb9aeb1d669f72dd30fa2440052818656e658c2
virak Sim <simvirak210721@gmail.com>
2024-11-06 14:40:30 +0700

    feat:investment-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,15 @@ import OurSuccessSection from '@/component/module/home-page/our-success-section'
 import ProjectSection from '@/component/module/home-page/project-section'
 import OurJourneySection from '@/component/module/home-page/our-journey-section'
 import BlogSection from '@/component/module/home-page/blogs-section'
+import InvestmentSection from '@/component/module/home-page/investment-section'
+import { fetchCategory } from '@/fetcher/category/fetch-category'
 export const revalidate = 300
 
 export default async function Home() {
   const heroData = await fetchHero()
   const blogsData = await fetchBlogs({ limit: 5 })
   const projectsData = await fetchProject({ limit: 10 })
+  const categoryData = await fetchCategory()
   const successData = await fetchSuccess()
 
   return (
@@ -20,6 +23,7 @@ export default async function Home() {
       <HeroSection data={heroData} />
       <OurSuccessSection data={successData} />
       <ProjectSection data={projectsData} />
+      <InvestmentSection data={categoryData} />
       <OurJourneySection />
       <BlogSection data={blogsData} />
     </div>
