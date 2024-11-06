ed1297d392339f4da24621d4cb0840b6e1fddbaa
Sovanrath <sovanrathsoem@gmail.com>
2024-11-06 13:56:58 +0700

    fix : font

--- src/constants/journey.ts
+++ src/constants/journey.ts
@@ -5,22 +5,22 @@ export const JOURNEY_DATA = {
     Founded key subsidiaries: Al Farm, Laoka, Factory Al, and UAVx`,
   },
   '2022': {
-    title: 'STRENGTHENING PARTNERSHIPS',
+    title: 'Strengthening Partnerships',
     description: ` Founding of three other subsidiaries (Dreamslab, Chiptree, and ByteFarm)
      Getting into a strategic partnership with STS Robotics`,
   },
   '2023': {
-    title: 'EXPANSION & NEW VENTURES',
+    title: 'Expansion & New Ventures',
     description: ` Launched additional subsidiary: Newfoundland 
     (focused on real estate development & consulting services)`,
   },
   '2024': {
-    title: 'FOCUS ON STRATEGIC GROWTH',
+    title: 'Focus On Strategic Growth',
     description: ` Prioritizing joint-ventures and partnership investments 
     to fuel expansion and market reach`,
   },
-  BEYOND: {
-    title: 'INNOVATION & GLOBAL REACH',
+  Beyond: {
+    title: 'Innovation & Global Reach',
     description: ` Exploring future investment opportunities both locally & 
     globally Seeking innovative, tech-driven solutions for sustainable growth`,
   },
